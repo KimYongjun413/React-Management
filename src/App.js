faf203df1cd22a62d9760aaece5e49e9e35e7dce
kimyongjun413 <kimyongjun413@gmail.com>
2019-05-08 21:42:27 +0900

    단일 고객 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = {
+  'name':'홍길동',
+  'birthday':'880413',
+  'gender':'남자',
+  'job':'회사원'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
-      </div>
+        <Customer
+            name={customer.name}
+            birthday={customer.birthday}
+            gender={customer.gender}
+            job={customer.job}
+        /> 
     );
   }
 }
