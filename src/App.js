f6f648956253b79fdf0585a49f11af1a241fae7d
kimyongjun413 <kimyongjun413@gmail.com>
2019-05-08 23:11:44 +0900

    다수 고객 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'id':'1',
+const customers = [
+{
+  'id':1,
   'image':'https://placeimg.com/64/64/any',
-  'name':'홍길동',
+  'name':'김용준',
   'birthday':'880413',
   'gender':'남자',
-  'job':'회사원'
+  'job':'프로그래머'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'이순신',
+  'birthday':'880414',
+  'gender':'남자',
+  'job':'디자이너'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'홍길동',
+  'birthday':'880415',
+  'gender':'남자',
+  'job':'프로그래머'
 }
+]
 
 class App extends Component {
   render() {
     return (
-        <Customer
-            id={customer.id}
-            image={customer.image}
-            name={customer.name}
-            birthday={customer.birthday}
-            gender={customer.gender}
-            job={customer.job}
-        /> 
+      <div>
+        { customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})}
+      </div>
     );
   }
 }
