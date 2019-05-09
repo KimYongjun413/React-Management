f6fc3c9efa2d5fe99f16949db968b18aea5768df
kimyongjun413 <kimyongjun413@gmail.com>
2019-05-09 23:54:02 +0900

    Customer Component Destructuring

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -3,14 +3,15 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 class Customer extends React.Component {
     render() {
+        const {id, image, name, birthday, gender, job} = this.props;
         return (
             <TableRow>
-                <TableCell>{this.props.id}</TableCell>
-                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
-                <TableCell>{this.props.name}</TableCell>
-                <TableCell>{this.props.birthday}</TableCell>
-                <TableCell>{this.props.gender}</TableCell>
-                <TableCell>{this.props.job}</TableCell>                
+                <TableCell>{id}</TableCell>
+                <TableCell><img src={image} alt="profile"/></TableCell>
+                <TableCell>{name}</TableCell>
+                <TableCell>{birthday}</TableCell>
+                <TableCell>{gender}</TableCell>
+                <TableCell>{job}</TableCell>                
             </TableRow>
         )
     }
